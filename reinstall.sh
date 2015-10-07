b659350620760c94cd03c2cb4aefdfdf00d14c76
Jeff Lyon <jeff@albatrossdigital.com>
2015-10-06 23:59:13 -0700

    Updating reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -1,9 +1,9 @@
-drush @proud.local site-install proud --db-url=mysql://drupal:drupal@database/drupal --sites-subdir=default -y  > ~/Downloads/a.txt
+drush @proud.test site-install proud --db-url=mysql://drupal:drupal@database/drupal --sites-subdir=default -y  > ~/Downloads/a.txt
 
-drush @proud.local user-create test --mail="test@test.com" --password="letmein"
-drush @proud.local user-add-role "administrator" --mail="test@test.com"
-drush @proud.local en -y proud_agency  proud_document  proud_event  proud_faq  proud_landingpage  proud_news  proud_people  proud_search  proud_social  proud_webform
+drush @proud.test user-create test --mail="test@test.com" --password="letmein"
+drush @proud.test user-add-role "administrator" --mail="test@test.com"
+drush @proud.test en -y proud_agency  proud_document  proud_event  proud_faq  proud_landingpage  proud_news  proud_people  proud_search  proud_social  proud_webform
 
-drush @proud.local uli >> /dev/null
-drush @proud.local uli test --browser=firefox >> /dev/null
+drush @proud.test uli >> /dev/null
+drush @proud.test uli test --browser=firefox >> /dev/null
 
