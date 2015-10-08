05f22ac3223e3412a82c3a1d5fdb9a8ada8d3271
Jeff Lyon <jeff@albatrossdigital.com>
2015-10-08 00:43:55 -0700

    Making the pull all command a generic bash command

---
+++ cmd.sh
@@ -0,0 +1,11 @@
+# using
+#  bash pull_all.sh
+
+cmd=${1:-"git pull"}
+dir=`pwd`
+echo $cmd
+
+for d in $dir/www/sites/all/modules/apps/* ; do (echo $d; cd $d; $cmd; echo;echo "--";echo); done
+for d in $dir/www/sites/all/modules/custom/* ; do (echo $d; cd $d; $cmd; echo;echo "--";echo); done
+for d in $dir/www/sites/all/modules/dev/* ; do (echo $d; cd $d; $cmd; echo;echo "--";echo); done
+for d in $dir/www/sites/all/themes/dev* ; do (echo $d; cd $d; $cmd; echo;echo "--";echo); done
