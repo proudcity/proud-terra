66df2a3f88d8f27e542999bd123a0263a0507fa0
Jeff Lyon <jeff@albatrossdigital.com>
2015-10-08 00:45:02 -0700

    Cleanup

--- pull_all.sh
+++
@@ -1,7 +0,0 @@
-# using
-#  bash pull_all.sh
-
-for d in ~/Apps/proud/local/www/sites/all/modules/apps/* ; do (echo $d; cd $d; git pull); done
-for d in ~/Apps/proud/local/www/sites/all/modules/custom/* ; do (echo $d; cd $d; git pull); done
-for d in ~/Apps/proud/local/www/sites/all/modules/dev/* ; do (echo $d; cd $d; git pull); done
-for d in ~/Apps/proud/local/www/sites/all/themes/dev* ; do (echo $d; cd $d; git pull); done
