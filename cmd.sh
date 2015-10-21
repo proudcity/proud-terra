8b1e7a4ebbbcf88184d1339832ff21d539f351b4
Jeff Lyon <jeff@albatrossdigital.com>
2015-10-20 22:01:05 -0700

    Adding profile repo

--- cmd.sh
+++ cmd.sh
@@ -5,7 +5,9 @@ cmd=${1:-"git pull"}
 dir=`pwd`
 echo $cmd
 
+cd $dir/www/profiles/proud/;git pull
 for d in $dir/www/sites/all/modules/apps/* ; do (echo $d; cd $d; $cmd; echo;echo "--";echo); done
 for d in $dir/www/sites/all/modules/custom/* ; do (echo $d; cd $d; $cmd; echo;echo "--";echo); done
 for d in $dir/www/sites/all/modules/dev/* ; do (echo $d; cd $d; $cmd; echo;echo "--";echo); done
-for d in $dir/www/sites/all/themes/dev* ; do (echo $d; cd $d; $cmd; echo;echo "--";echo); done
+for d in $dir/www/sites/all/themes/dev/* ; do (echo $d; cd $d; $cmd; echo;echo "--";echo); done
+
