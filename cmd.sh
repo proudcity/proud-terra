5aa17b93645456364d35b93eb7d35c4a6d912ff0
Jeff Lyon <jeff@albatrossdigital.com>
2015-10-26 17:22:07 -0700

    Small improvement to cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -5,6 +5,7 @@ cmd=${1:-"git pull"}
 dir=`pwd`
 echo $cmd
 
+echo $dir/www/profiles/proud/
 cd $dir/www/profiles/proud/;git pull
 for d in $dir/www/sites/all/modules/apps/* ; do (echo $d; cd $d; $cmd; echo;echo "--";echo); done
 for d in $dir/www/sites/all/modules/custom/* ; do (echo $d; cd $d; $cmd; echo;echo "--";echo); done
